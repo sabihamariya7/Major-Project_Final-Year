c74222a7a061970138e01d8b79c960dfc5b8e0b4
sabihamariya7 <67388431+sabihamariya7@users.noreply.github.com>
2021-04-25 21:16:54 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,226 @@
+import warnings
+warnings.filterwarnings("ignore")
+import numpy as np
+import pandas as pd
+import pickle
+import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+from bs4 import BeautifulSoup
+from fuzzywuzzy import fuzz
+import distance
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.preprocessing import MinMaxScaler
+import spacy
+from tqdm import tqdm
+import streamlit as st
+
+st.set_page_config(page_title='Quora Similarity Predictor')
+st.title('Quora Similarity Predictor')
+st.write("""
+Below is our Questions Similarity Predictor for Quora
+""")
+
+q1 = st.text_input('Enter Question 1...')
+q2 = st.text_input('Enter Question 2...')
+
+pkl_filename = "model/quora_similarity.pkl"
+with open(pkl_filename, 'rb') as file:
+    pickle_model = pickle.load(file)
+	
+nltk.download('stopwords')
+# To get the results in 4 decemal points
+SAFE_DIV = 0.0001 
+STOP_WORDS = stopwords.words("english")
+def preprocess2(x):
+    x = str(x).lower()
+    x = x.replace(",000,000", "m").replace(",000", "k").replace("′", "'").replace("’", "'")\
+                           .replace("won't", "will not").replace("cannot", "can not").replace("can't", "can not")\
+                           .replace("n't", " not").replace("what's", "what is").replace("it's", "it is")\
+                           .replace("'ve", " have").replace("i'm", "i am").replace("'re", " are")\
+                           .replace("he's", "he is").replace("she's", "she is").replace("'s", " own")\
+                           .replace("%", " percent ").replace("₹", " rupee ").replace("$", " dollar ")\
+                           .replace("€", " euro ").replace("'ll", " will")
+    x = re.sub(r"([0-9]+)000000", r"\1m", x)
+    x = re.sub(r"([0-9]+)000", r"\1k", x)
+    porter = PorterStemmer()
+    pattern = re.compile('\W')
+    if type(x) == type(''):
+        x = re.sub(pattern, ' ', x)
+    if type(x) == type(''):
+        x = porter.stem(x)
+        example1 = BeautifulSoup(x)
+        x = example1.get_text()
+    return x
+
+def get_token_features2(q1, q2):
+    token_features = [0.0]*10
+    # Converting the Sentence into Tokens: 
+    q1_tokens = q1.split()
+    q2_tokens = q2.split()
+    if len(q1_tokens) == 0 or len(q2_tokens) == 0:
+        return token_features
+    # Get the non-stopwords in Questions
+    q1_words = set([word for word in q1_tokens if word not in STOP_WORDS])
+    q2_words = set([word for word in q2_tokens if word not in STOP_WORDS])
+    #Get the stopwords in Questions
+    q1_stops = set([word for word in q1_tokens if word in STOP_WORDS])
+    q2_stops = set([word for word in q2_tokens if word in STOP_WORDS])
+    # Get the common non-stopwords from Question pair
+    common_word_count = len(q1_words.intersection(q2_words))
+    # Get the common stopwords from Question pair
+    common_stop_count = len(q1_stops.intersection(q2_stops))
+    # Get the common Tokens from Question pair
+    common_token_count = len(set(q1_tokens).intersection(set(q2_tokens)))
+    token_features[0] = common_word_count / (min(len(q1_words), len(q2_words)) + SAFE_DIV)
+    token_features[1] = common_word_count / (max(len(q1_words), len(q2_words)) + SAFE_DIV)
+    token_features[2] = common_stop_count / (min(len(q1_stops), len(q2_stops)) + SAFE_DIV)
+    token_features[3] = common_stop_count / (max(len(q1_stops), len(q2_stops)) + SAFE_DIV)
+    token_features[4] = common_token_count / (min(len(q1_tokens), len(q2_tokens)) + SAFE_DIV)
+    token_features[5] = common_token_count / (max(len(q1_tokens), len(q2_tokens)) + SAFE_DIV)
+    # Last word of both question is same or not
+    token_features[6] = int(q1_tokens[-1] == q2_tokens[-1])
+    # First word of both question is same or not
+    token_features[7] = int(q1_tokens[0] == q2_tokens[0])
+    token_features[8] = abs(len(q1_tokens) - len(q2_tokens))
+    #Average Token Length of both Questions
+    token_features[9] = (len(q1_tokens) + len(q2_tokens))/2
+    return token_features
+
+# get the Longest Common sub string
+def get_longest_substr_ratio2(a, b):
+    strs = list(distance.lcsubstrings(a, b))
+    if len(strs) == 0:
+        return 0
+    else:
+        return len(strs[0]) / (min(len(a), len(b)) + 1)
+
+def extract_features2(df):
+    # preprocessing each question
+    df["question1"] = df["question1"].fillna("").apply(preprocess2)
+    df["question2"] = df["question2"].fillna("").apply(preprocess2)
+    # Merging Features with dataset
+    token_features = df.apply(lambda x: get_token_features2(x["question1"], x["question2"]), axis=1)
+    df["cwc_min"]       = list(map(lambda x: x[0], token_features))
+    df["cwc_max"]       = list(map(lambda x: x[1], token_features))
+    df["csc_min"]       = list(map(lambda x: x[2], token_features))
+    df["csc_max"]       = list(map(lambda x: x[3], token_features))
+    df["ctc_min"]       = list(map(lambda x: x[4], token_features))
+    df["ctc_max"]       = list(map(lambda x: x[5], token_features))
+    df["last_word_eq"]  = list(map(lambda x: x[6], token_features))
+    df["first_word_eq"] = list(map(lambda x: x[7], token_features))
+    df["abs_len_diff"]  = list(map(lambda x: x[8], token_features))
+    df["mean_len"]      = list(map(lambda x: x[9], token_features))
+    #Computing Fuzzy Features and Merging with Dataset
+    # do read this blog: http://chairnerd.seatgeek.com/fuzzywuzzy-fuzzy-string-matching-in-python/
+    # https://stackoverflow.com/questions/31806695/when-to-use-which-fuzz-function-to-compare-2-strings
+    # https://github.com/seatgeek/fuzzywuzzy
+    df["token_set_ratio"]       = df.apply(lambda x: fuzz.token_set_ratio(x["question1"], x["question2"]), axis=1)
+    # The token sort approach involves tokenizing the string in question, sorting the tokens alphabetically, and 
+    # then joining them back into a string We then compare the transformed strings with a simple ratio().
+    df["token_sort_ratio"]      = df.apply(lambda x: fuzz.token_sort_ratio(x["question1"], x["question2"]), axis=1)
+    df["fuzz_ratio"]            = df.apply(lambda x: fuzz.QRatio(x["question1"], x["question2"]), axis=1)
+    df["fuzz_partial_ratio"]    = df.apply(lambda x: fuzz.partial_ratio(x["question1"], x["question2"]), axis=1)
+    df["longest_substr_ratio"]  = df.apply(lambda x: get_longest_substr_ratio2(x["question1"], x["question2"]), axis=1)
+    return df
+
+def normalized_word_Common2(row):
+    w1 = set(map(lambda word: word.lower().strip(), row['question1'].split(" ")))
+    w2 = set(map(lambda word: word.lower().strip(), row['question2'].split(" ")))    
+    return 1.0 * len(w1 & w2)
+
+def normalized_word_Total2(row):
+    w1 = set(map(lambda word: word.lower().strip(), row['question1'].split(" ")))
+    w2 = set(map(lambda word: word.lower().strip(), row['question2'].split(" ")))    
+    return 1.0 * (len(w1) + len(w2))
+
+def normalized_word_share2(row):
+    w1 = set(map(lambda word: word.lower().strip(), row['question1'].split(" ")))
+    w2 = set(map(lambda word: word.lower().strip(), row['question2'].split(" ")))    
+    return 1.0 * len(w1 & w2)/(len(w1) + len(w2))
+
+# en_vectors_web_lg, which includes over 1 million unique vectors.
+def get_vectors2(data, question_1, question_2):
+    nlp = spacy.load('en_core_web_sm')
+    vecs1 = []
+    vecs2 = []
+    # https://github.com/noamraph/tqdm
+    # tqdm is used to print the progress bar
+    for qu1 in tqdm(list(question_1)):
+        doc1 = nlp(qu1) 
+        mean_vec1 = np.zeros([len(doc1), len(doc1[0].vector)])
+        for word1 in doc1:
+            # word2vec
+            vec1 = word1.vector
+            # fetch df score
+            try:
+                idf = word2tfidf[str(word1)]
+            except:
+                idf = 0
+            # compute final vec
+            mean_vec1 += vec1 * idf
+        mean_vec1 = mean_vec1.mean(axis=0)
+        vecs1.append(mean_vec1)
+
+    for qu2 in tqdm(list(question_2)):
+        doc2 = nlp(qu2) 
+        mean_vec2 = np.zeros([len(doc2), len(doc2[0].vector)])
+        for word2 in doc2:
+            # word2vec
+            vec2 = word2.vector
+            # fetch df score
+            try:
+                idf = word2tfidf[str(word2)]
+            except:
+                #print word
+                idf = 0
+            # compute final vec
+            mean_vec2 += vec2 * idf
+        mean_vec2 = mean_vec2.mean(axis=0)
+        vecs2.append(mean_vec2)
+    df_q1 = pd.DataFrame(vecs1, index= data.index)
+    df_q2 = pd.DataFrame(vecs2, index= data.index)
+    df_q1.columns=[str(x)+'_x' for x in df_q1.columns]
+    df_q2.columns=[str(y)+'_y' for y in df_q2.columns]
+    return df_q1,df_q2
+
+def compute_similarity2(q1,q2):
+    questions={'question1':[q1],'question2':[q2]}
+    df=pd.DataFrame(questions)
+    df['id']=pd.DataFrame(list(df.index))
+    df = extract_features2(df) 
+    df['q1len'] = df['question1'].str.len()
+    df['q2len'] = df['question2'].str.len()
+    df['q1_n_words'] = df['question1'].apply(lambda row: len(row.split(" ")))
+    df['q2_n_words'] = df['question2'].apply(lambda row: len(row.split(" ")))
+    df['word_Common'] = df.apply(normalized_word_Common2, axis=1)
+    df['word_Total'] = df.apply(normalized_word_Total2, axis=1)
+    df['word_share'] = df.apply(normalized_word_share2, axis=1)
+    df['question1'] = df['question1'].apply(lambda x: str(x))
+    df['question2'] = df['question2'].apply(lambda x: str(x))
+    # merge texts
+    questions = list(df['question1']) + list(df['question2'])
+    tfidf = TfidfVectorizer(lowercase=False, )
+    tfidf.fit_transform(questions)
+    # dict key:word and value:tf-idf score
+    word2tfidf = dict(zip(tfidf.get_feature_names(), tfidf.idf_))
+    df_q1,df_q2=get_vectors2(data=df, question_1=df['question1'], question_2=df['question2'])
+    df_q1['id']=df['id']
+    df_q2['id']=df['id']
+    df = df.merge(df_q1, on='id',how='inner')
+    df = df.merge(df_q2, on='id',how='inner')
+    X=df.drop(['id','question1','question2'], axis=1).fillna(0)
+    return X
+
+status=st.button('Check Similarity')
+if status:
+    X=compute_similarity2(q1,q2)
+    result=pickle_model.predict(X)[0]
+    probability=np.max(pickle_model.predict_proba(X))
+    if(result==0):
+        st.subheader(f"Both the questions are different with the probability of {probability*100:.2f}%.")
+    else:
+        st.subheader(f"Both the questions are similar with the probability of {probability*100:.2f}%.")
+
